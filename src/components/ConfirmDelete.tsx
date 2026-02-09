ce80cb8d322b81d91ef889fedc5c0e9cdf90d4ba
stanislavluft <elemsyk@gmail.com>
2026-02-09 18:23:23 +0500

    ui: implement confirmDelete styles

--- src/components/ConfirmDelete.tsx
+++ src/components/ConfirmDelete.tsx
@@ -6,10 +6,25 @@ interface ConfirmDeleteProps {
 function ConfirmDelete({ onCancel, onConfirm }: ConfirmDeleteProps) {
   return (
     <>
-      <h3>Вы уверены?</h3>
-      <div>
-        <button onClick={onConfirm}>Да</button>
-        <button onClick={onCancel}>Нет</button>
+      <div className="flex flex-col items-center text-center">
+        <h3 className="mt-3 text-lg font-bold">Удалить операцию?</h3>
+        <p className="mt-3 mb-6 max-w-3xs text-xs text-balance">
+          Это действие необратимо. Операция будет удалена из вашей истории.
+        </p>
+        <div className="flex w-full gap-2">
+          <button
+            className="flex-1 rounded-4xl bg-zinc-100 py-2.5 text-zinc-900 shadow-md transition-all duration-150 active:text-zinc-500"
+            onClick={onCancel}
+          >
+            Отменить
+          </button>
+          <button
+            className="flex-1 rounded-4xl bg-zinc-100 py-2.5 text-red-600 shadow-md transition-all duration-150 active:text-red-400"
+            onClick={onConfirm}
+          >
+            Удалить
+          </button>
+        </div>
       </div>
     </>
   );
