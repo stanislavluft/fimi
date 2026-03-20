9b3c023c7cbae57f778359122dfdb42307a9e3a8
stanislavluft <elemsyk@gmail.com>
2026-03-20 20:03:20 +0500

    feat: add CardBalance in Dashboard

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,7 +1,10 @@
+import CardBalance from "@/components/finance/CardBalance";
+
 function DashboardPage() {
   return (
     <section>
       <h1 className="text-2xl font-semibold">Dashboard</h1>
+      <CardBalance/>
     </section>
   );
 }
