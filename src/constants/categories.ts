2e6f7cc2e520c196ccee34a3023d10e0a37b3e97
stanislavluft <elemsyk@gmail.com>
2026-04-14 13:14:29 +0500

    style: change lang categories to eng

--- src/constants/categories.ts
+++ src/constants/categories.ts
@@ -12,11 +12,11 @@ import type { LucideIcon } from 'lucide-react';
 import type { OperationType } from './operation-types';
 
 export const BASE_CATEGORIES = [
-  { id: 'x7k2', label: 'Зарплата', type: 'income', icon: BriefcaseBusiness },
-  { id: 'b3qw', label: 'Еда', type: 'expense', icon: ShoppingBag },
-  { id: 'n6ht', label: 'Транспорт', type: 'expense', icon: BusFront },
-  { id: 'r4vz', label: 'Жилье', type: 'expense', icon: House },
-  { id: 'c8yd', label: 'Здоровье', type: 'expense', icon: HeartPulse },
-  { id: 'j1ls', label: 'Развлечения', type: 'expense', icon: Film },
-  { id: 'g5ek', label: 'Другое', type: 'expense', icon: CircleEllipsis },
+  { id: 'x7k2', label: 'Salary', type: 'income', icon: BriefcaseBusiness },
+  { id: 'b3qw', label: 'Food', type: 'expense', icon: ShoppingBag },
+  { id: 'n6ht', label: 'Transport', type: 'expense', icon: BusFront },
+  { id: 'r4vz', label: 'Housing', type: 'expense', icon: House },
+  { id: 'c8yd', label: 'Health', type: 'expense', icon: HeartPulse },
+  { id: 'j1ls', label: 'Entertainment', type: 'expense', icon: Film },
+  { id: 'g5ek', label: 'Other', type: 'expense', icon: CircleEllipsis },
 ] as const satisfies { id: string; label: string; type: OperationType; icon: LucideIcon }[];
