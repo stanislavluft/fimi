ea0de482900ff8299bbca28b549a243003aff4c3
stanislavluft <elemsyk@gmail.com>
2026-04-20 14:38:55 +0500

    style: change warning text style and use variant outline in button

--- src/components/shared/ConfirmDelete.tsx
+++ src/components/shared/ConfirmDelete.tsx
@@ -8,11 +8,11 @@ interface ConfirmDeleteProps {
 function ConfirmDelete({ onCancel, onConfirm }: ConfirmDeleteProps) {
   return (
     <div className="flex flex-col">
-      <h3 className="my-3 ml-2 text-sm text-balance">
+      <h3 className="text-muted-foreground my-3 ml-2 text-sm text-balance">
         This action is irreversible. The operation will be removed from your history.
       </h3>
       <div className="flex items-center justify-end gap-2">
-        <Button variant="ghost" onClick={onCancel} size="lg">
+        <Button variant="outline" onClick={onCancel} size="lg">
           Cancel
         </Button>
         <Button variant="destructive" onClick={onConfirm} size="lg">
