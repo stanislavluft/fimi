3a0c099ca4f465a30b2442e219733c943ed3fe31
stanislavluft <elemsyk@gmail.com>
2026-04-20 14:00:01 +0500

    style: change errors lang to eng

--- src/schemas/operation-schema.ts
+++ src/schemas/operation-schema.ts
@@ -5,13 +5,15 @@ import { OPERATION_TYPES } from '@/constants/operation-types';
 export const operationValues = OPERATION_TYPES.map((op) => op.value) as [string, ...string[]];
 
 export const operationSchema = z.object({
-  amount: z.number({ error: 'Введите сумму' }).positive({ error: 'Сумма должна быть больше нуля' }),
+  amount: z
+    .number({ error: 'Enter amount' })
+    .positive({ error: 'The amount must be greater than zero' }),
 
-  categoryId: z.string({ error: 'Выберите категорию' }),
+  categoryId: z.string({ error: 'Select Category' }),
 
-  date: z.string({ error: 'Выберите дату' }).min(1, { error: 'Выберите дату' }),
+  date: z.string({ error: 'Select Date' }).min(1, { error: 'Select Date' }),
 
-  time: z.string({ error: 'Укажите время' }).min(1, { error: 'Выберите дату' }),
+  time: z.string({ error: 'Specify time' }).min(1, { error: 'Specify time' }),
 
   comment: z.string().optional(),
 });
